76f0a57c4552015557d626555d9078a86928919a
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-03-29 22:06:44 -0300

    exercicio_es6

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-function Animal(nome) {
-    if (this.constructor === Animal) {
-        throw new Error("Você não pode instanciar um objeto da classe abstrata Animal diretamente.");
-    }
-    this.nome = nome;
+const alunos = [
+    { nome: 'kevin', nota: 10 },
+    { nome: 'reis', nota: 9 },
+    { nome: 'hartwig', nota: 8 },
+    { nome: 'naruto', nota: 5 },
+    { nome: 'boruto', nota: 3 }
+];
+
+function alunosAprovados(alunos) {
+    return alunos.filter(function(aluno) {
+        return aluno.nota >= 6;
+    });
 }
 
-function Cachorro(nome, raca) {
-    Animal.call(this, nome);
-    this.raca = raca;
-}
-
-function Gato(nome, cor) {
-    Animal.call(this, nome);
-    this.cor = cor;
-}
-
-const cachorro1 = new Cachorro("Akira", "husky siberiano");
-const gato1 = new Gato("Tom", "Branco");
-const cachorro2 = new Cachorro("Caramelo", "Pastor Alemão");
-console.log(cachorro3);
+console.log(alunosAprovados(alunos));
