f4beb9a93407dc9411674e2820de79fb9a815aee
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-03-29 23:16:00 -0300

    exercicio_ts

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(number1, number2) {
+    return number1 * number2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('Kevin'));
+console.log(multiplicacao(5, 10));
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicacao(number1: number, number2: number){
+    return number1 * number2;
+}
+
+function saudacao(nome: string){
+    return `Olá ${nome}`;
+}
+
+console.log(saudacao('Kevin'));
+console.log(multiplicacao(5,10));
+
